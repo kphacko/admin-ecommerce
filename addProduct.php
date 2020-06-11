eeb344acbbc21a137b07c9e690f2ebafbb57d2c2
Naitik Raval <46623484+naitik41200@users.noreply.github.com>
2020-06-11 22:34:58 +0530

    addProduct

--- addProduct.php
+++ addProduct.php
@@ -4,26 +4,70 @@ include_once('header.php');
 
 
 <div id="content" class="main-content">
-<div class="container mt-5">
+<div class="container ">
+<div class="col-sm-10">
 <form method="post">
            <div class="form-group">
-               <p>Enter product titile</p>
-                <input id="t-text" type="text" name="txt" placeholder="titile" class="form-control" required>
+               <p>Enter Diamond Weight</p>
+                <input id="t-text" type="number" name="txt" placeholder="Diamond Weight" class="form-control" required>
             </div>
             <div class="form-group">
-               <p>Enter product discription</p>
-                <input id="t-text" type="text" name="txt" placeholder="Discription" class="form-control" required>
+               <p>Enter Gold Weight</p>
+                <input id="t-text" type="number" name="txt" placeholder="Gold Weight" class="form-control" required>
                 
             </div>
             <div class="form-group">
-               <p>Enter product parameters</p>
-                <input id="t-text" type="text" name="txt" placeholder="parameters" class="form-control" required>
+               <p>Enter Diamond Count</p>
+               <input id="t-text" type="number" name="txt" placeholder="Count" class="form-control" required>
             </div>
+            <div class="form-group">
+               <p>Enter Manufacturer</p>
+               <input id="t-text" type="text" name="txt" placeholder="Manufacturer" class="form-control" required>
+            </div>
+            <div class="form-group">
+               <p>Enter Diamond Sieve</p>
+               <input id="t-text" type="text" name="txt" placeholder="Diamond Sieve" class="form-control" required>
+            </div>
+            
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+$(document).ready(function() {
+    var max_fields = 10;
+    var wrapper = $(".container1");
+    var add_button = $(".add_form_field");
 
-        
+    var x = 1;
+    $(add_button).click(function(e) {
+        e.preventDefault();
+        if (x < max_fields) {
+            x++;
+            $(wrapper).append('<div><input id="t-text" type="text" name="mytext[]" placeholder="Diamond Sieve" class="form-control" required><a href="#" class="delete btn btn-danger">Delete</a></div>'); //add input box
+        } else {
+            alert('You Reached the limits')
+        }
+    });
+
+    $(wrapper).on("click", ".delete", function(e) {
+        e.preventDefault();
+        $(this).closest('div').remove();
+        x--;
+    })
+});
+</script>
+<div class="container1 form-group">
+    <button class="add_form_field btn btn-primary">New Diamond Sieve &nbsp; 
+      <span style="font-size:16px; font-weight:bold;">+ </span>
+    </button>
+    
+</div>
+<div class="form-group">
+               <p>Enter Design Dimensions</p>
+               <input id="t-text" type="number" name="txt" placeholder="Dimensions" class="form-control" required>
+</div>
 
 <input  value="submit" type="submit" name="txt" class="mt-4 btn btn-primary">
 </form>
+</div>
 </div>
 </div>
 
