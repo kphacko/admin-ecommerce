5ef01730b1a82ba4dddf5fc0f9aea0027c73a246
Naitik Raval <46623484+naitik41200@users.noreply.github.com>
2020-06-13 15:07:14 +0530

    copy clipboard

--- uploadImage.php
+++ uploadImage.php
@@ -10,10 +10,15 @@ if ($_GET['img']!=NULL) {
       
 // print_r($img);
 echo '
-<div class="container">
-  <h2>Image Gallery</h2>
- 
-  <div class="row">
+
+<h2>Image Gallery</h2>
+<div class="container row input-group">
+  <input class="form-control input-sm input-monospace" value="Click On Any Image For Link" type="text" id="imgsrc">
+  <button class="btn btn-sm" onclick="fuc()"><svg class="octicon octicon-clippy" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M5.75 1a.75.75 0 00-.75.75v3c0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75v-3a.75.75 0 00-.75-.75h-4.5zm.75 3V2.5h3V4h-3zm-2.874-.467a.75.75 0 00-.752-1.298A1.75 1.75 0 002 3.75v9.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0014 13.25v-9.5a1.75 1.75 0 00-.874-1.515.75.75 0 10-.752 1.298.25.25 0 01.126.217v9.5a.25.25 0 01-.25.25h-8.5a.25.25 0 01-.25-.25v-9.5a.25.25 0 01.126-.217z"></path></svg></button>
+</div>
+
+<div class="container mt-4">
+  <div class="row ">
   ';
   
   foreach ($img as $key => $value) {
@@ -21,21 +26,14 @@ echo '
 
   echo '
     <div class="col-md-4">
-      <div class="thumbnail">
-        <a href="'.$img[$key][0].'" target="_blank">
+      <div class="thumbnail m-2">
         <picture>
     <source type="image/webp" srcset="'.$img[$key][0].'">
-    
-    <img src="'.$img[$key][0].'" alt="Lights" style="width:100%">
+    <img src="'.$img[$key][0].'" alt="Lights" style="width:100%" onclick="selectAvatar(this)">
 </picture>
-
-          
-         
-        </a>
       </div>
     </div>
    ';
-
   }
   echo '</div>
 </div>
@@ -44,9 +42,6 @@ echo '
 ?>
 <div class="container mt-5">
 <form action="inc/uploadImg.php" method="POST" enctype="multipart/form-data">
-
-           
-
  <div class="custom-file-container form-group" data-upload-id="mySecondImage">
     <label>Upload (Allow Multiple) <a href="javascript:void(0)" class="custom-file-container__image-clear" title="Clear Image">x</a></label>
     <label class="custom-file-container__custom-file" >
@@ -55,9 +50,7 @@ echo '
         <span class="custom-file-container__custom-file__custom-file-control"></span>
     </label>
     <div class="custom-file-container__image-preview">
-
     </div>
-
 </div>
 
 <button class="btn btn-primary mb-2" value="submit" type="submit">Submit</button>
@@ -65,14 +58,23 @@ echo '
 </div>
 </div>
 
-
-
-
-
-
-
-
-
+<html>
+<body>
+  <script>
+  var imgScr;
+  function selectAvatar(el) {
+    imgSrc = el.src;
+    var inputF = document.getElementById("imgsrc");
+    inputF.value = imgSrc;
+    }
+  function fuc() { 
+    var copyText = document.getElementById("imgsrc");
+    copyText.select(); 
+    document.execCommand("copy"); 
+    } 
+  </script>
+</body>
+</html>
 
 
 
